fixed training masking

UAT/datasets/data.py
@@ -581,19 +581,19 @@ def openml_ds(
 
     if train_complete and test_complete:
         X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=split, random_state=key)
-        
+
     elif train_complete and not test_complete: # TRAIN COMPLETE IS TRUE AND TEST COMPLETE IS FALSE
         X_train, X, y_train, y_valid = train_test_split(X_train, y_train, test_size=split, random_state=key)
-    
-    elif not train_complete and test_complete:        
+
+    elif not train_complete and test_complete:
         X, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=split, random_state=key)
-    
+
     elif not train_complete and not test_complete:
         X = X_train
 
     # create missingness mask
     cols = X_train.shape[1]
-    if missing == "MCAR": 
+    if missing == "MCAR":
         cols_miss = np.minimum(cols, cols_miss) # clip cols missing 
         rand_arr = rng.uniform(0, 1, X.shape)
         nan_arr = np.where(rand_arr < prop, np.nan, 1.0)
@@ -604,9 +604,6 @@ def openml_ds(
         cols_miss = np.minimum(cols, cols_miss) # clip cols missing
         nan_list = []
         for i,b in enumerate(cat_bin):
-            # for each column get ~20% that you are keeping for sure
-            # based completely on the value, hence MNAR
-            # aim to delete 60% per column
             if b == 1: # categorical
                 unique, counts = np.unique(X[:, i], return_counts=True)
                 if len(unique) == 2:

UAT/models/layers.py
@@ -242,18 +242,112 @@ def Dense(
                 W, b = params["hw"], params["hb"]
             else:
                 W, b = params
-            return jnp.einsum('...i,...ij->...j', x*mask, W) + b
+            return jnp.einsum('...i,...ij->...j', x, W*mask) + b
         else:
             if scan:
                 W = params["hw"]
             else:
                 W = params
-            return jnp.einsum('...i,...ij->...j', x*mask, W)
+            return jnp.einsum('...i,...ij->...j', x, W*mask)
     return init_fun, apply_fun
 
+def DenseRoll(
+    features,
+    in_dim,
+    out_dim,
+    bias = True,
+    W_init = glorot_uniform(),
+    b_init = zeros,
+    ):
+    def init_fun(
+        rng
+        ):
+        k1, k2, k3, k4 = random.split(rng, 4)
+        out_shape = (out_dim)
+        W1 = W_init(k1, (in_dim, out_dim))
+        W2 = W_init(k2, (features, out_dim))
+        if bias:
+            b1 = b_init(k3, (out_dim,))
+            b2 = b_init(k4, (out_dim,))
+            return (W1, W2, b1, b2)
+        else:
+            return (W1, W2)
+    def apply_fun(params, x, res, scan = False, mask = 1.0):
+        if bias:
+            if scan:
+                W1, W2, b1, b2 = params["hw1"], params["hb1"], params["hw2"], params["hb2"]
+            else:
+                W1, W2, b1, b2 = params
+            return (jnp.einsum('...i,...ij->...j', x, W1) + b1 +
+                    jnp.einsum('...i,...ij->...j', res*mask, W2) + b2)
+        else:
+            if scan:
+                W1, W2 = params["hw1"], params["hw2"]
+            else:
+                W1, W2 = params
+            return (jnp.einsum('...i,...ij->...j', x, W1) +
+                    jnp.einsum('...i,...ij->...j', res*mask, W2))
+    return init_fun, apply_fun
+
+def ResNet(
+    features,
+    hidden_dim,
+    out_dim,
+    num_hidden = 1,
+    W_init = glorot_uniform(),
+    b_init = zeros,
+    activation=relu
+    ):
+    init_l1, layer_1 = Dense(
+        features, hidden_dim, W_init=W_init, b_init=b_init)
+    init_hidden, layer_h = DenseRoll(
+        features, hidden_dim, hidden_dim, W_init=W_init, b_init=b_init)
+    init_out, layer_out = Dense(
+        hidden_dim, out_dim, W_init=W_init, b_init=b_init)
+
+    def init_fun(rng):
+        params = {}
+        rng, key = random.split(rng)
+        params["l1"] = init_l1(key)
+
+        hw1, hw2, hb1, hb2 = [], [], [], []
+        for _ in range(num_hidden):
+            rng, key = random.split(rng)
+            W1, W2, b1, b2 = init_hidden(key)
+            hw1.append(W1)
+            hw2.append(W2)
+            hb1.append(b1)
+            hb2.append(b2)
+        
+        params["hidden"] = {
+            "hw1":jnp.stack(hw1, axis=0),
+            "hw2":jnp.stack(hw2, axis=0),
+            "hb1":jnp.stack(hb1, axis=0),
+            "hb2":jnp.stack(hb2, axis=0)
+        }
+
+        rng, key = random.split(rng)
+        params["out"] = init_out(key)
+
+        return params
+    
+    def apply_fun(params, inputs, mask, rng=None):
+        h = activation(layer_1(params["l1"], inputs, mask=mask))
+
+        def body(carry, x):
+            # note x is actually the params
+            temp = activation(layer_h(x, carry, inputs, mask=mask, scan=True))
+            return temp, None
+        
+        h, _ = jax.lax.scan(body, h, params["hidden"])
+
+        out = layer_out(params["out"], h)
+        return out
+    
+    return init_fun, apply_fun
 
 def NeuralNetGeneral(
-    features, 
+    features,
     in_dim,
     hidden_dim,
     out_dim,
@@ -329,7 +423,7 @@ def NeuralNet(
         
         hw, hb = [], []
         for _ in range(num_hidden):
-            rng, key = random.split(rng)
+            rng, key = random.split(rng, 2)
             W, b = init_hidden(key)
             hw.append(W)
             hb.append(b)
@@ -356,9 +450,85 @@ def NeuralNet(
 
         out = layer_out(params["out"], h)
         return out
-    
+
     return init_fun, apply_fun
 
+def ConcDropNeuralNet(
+    in_dim,
+    hidden_dim,
+    out_dim,
+    num_hidden = 1,
+    W_init = glorot_uniform(),
+    b_init = zeros,
+    activation=relu,
+    temp = 0.1,
+    eps = 1e-7
+    ):
+    init_l1, layer_1 = Dense(
+        in_dim, hidden_dim, W_init=W_init, b_init=b_init)
+    init_hidden, layer_h = Dense(
+        hidden_dim, hidden_dim, W_init=W_init, b_init=b_init)
+    init_out, layer_out = Dense(
+        hidden_dim, out_dim, W_init=W_init, b_init=b_init)
+
+    def init_fun(rng):
+        params = {}
+        rng, key = random.split(rng)
+        params["l1"] = init_l1(key)
+        params["l1_logit"] = jnp.zeros((in_dim,hidden_dim))
+
+        hw, hb, lgts = [], [], []
+        for _ in range(num_hidden):
+            rng, key = random.split(rng, 2)
+            W, b = init_hidden(key)
+            hw.append(W)
+            hb.append(b)
+            lgts.append(jnp.zeros((hidden_dim,hidden_dim)))
+
+        params["hidden"] = {
+            "hw":jnp.stack(hw, axis=0),
+            "hb":jnp.stack(hb, axis=0),
+            "lgts":jnp.stack(lgts, axis=0)
+        }
+
+        rng, key = random.split(rng)
+        params["out"] = init_out(key)
+
+        return params
+
+    def masksamp(lgts, rng):
+        probs = jax.nn.sigmoid(lgts)
+        unif_noise = random.uniform(rng, lgts.shape)
+        drop_prob = (jnp.log(probs + eps)
+                        - jnp.log(1.0 - probs + eps)
+                        + jnp.log(unif_noise + eps)
+                        - jnp.log(1.0 - unif_noise + eps)
+                        )
+        drop_prob = jax.nn.sigmoid(drop_prob / temp)
+        # this line is 1.0 - drop_prob if you want to select more important variables
+        # during training
+        random_arr = 1. - drop_prob  # (1 if keeping 0 if removing)
+        return random_arr
+
+    def apply_fun(params, inputs, mask1, rng):
+
+        h = activation(layer_1(params["l1"], inputs, mask=mask1))
+
+        def body(carry, x):
+            # note x is actually the params
+            mask = masksamp(x["lgts"], x["rng"])
+            temp = activation(layer_h(x, carry, mask=mask, scan=True))
+            return temp, None
+
+        rng = random.split(rng, num_hidden)
+        params["hidden"]["rng"] = rng
+
+        h, _ = jax.lax.scan(body, h, params["hidden"])
+
+        out = layer_out(params["out"], h)
+        return out
+
+    return init_fun, apply_fun
 
 def LayerNorm(epsilon=1e-5, center=True, scale=True, beta_init=zeros, gamma_init=ones):
     def init_fun(rng, elements): 

UAT/models/models.py
@@ -5,7 +5,7 @@ from jax import random
 from jax.nn import (relu, log_softmax, softmax, softplus, sigmoid, elu,
                     leaky_relu, selu, gelu, normalize)
 from jax.nn.initializers import glorot_uniform, glorot_normal, normal, ones, zeros
-from .layers import (DenseGeneral, Dense, NeuralNet, NeuralNetGeneral, 
+from .layers import (DenseGeneral, Dense, ConcDropNeuralNet, NeuralNet, NeuralNetGeneral, 
                      AttentionBlock, AttentionLayer, LayerNorm, Embed, Bijector,
                      AttentionLayer2, AttentionBlock2)
 from itertools import combinations
@@ -121,11 +121,8 @@ def MaskedNeuralNet(
         eps = 1e-7,
         **kwargs
     ):
-    init_l1, l1 = Dense(
-        features, embed_hidden_size, bias = False, W_init = W_init, b_init = b_init,
-    )
-    init_fk, fk = NeuralNet(
-        embed_hidden_size, embed_hidden_size, d_model, embed_hidden_layers, W_init=W_init, b_init=b_init, activation=embed_activation
+    init_fk, fk = ConcDropNeuralNet(
+        features, embed_hidden_size, d_model, embed_hidden_layers, W_init=W_init, b_init=b_init, activation=embed_activation
     )
     init_g, g = NeuralNet(
         d_model, net_hidden_size, out_size, net_hidden_layers, W_init=W_init, b_init=b_init, activation=net_activation
@@ -133,9 +130,6 @@ def MaskedNeuralNet(
     def init_fun(rng):
 
         params = {}
-        rng, key = random.split(rng)
-        params["l1"] = init_l1(key)
-
         rng, key = random.split(rng)
         params["fk"] = init_fk(key)
 
@@ -147,16 +141,22 @@ def MaskedNeuralNet(
 
         return params
 
-    def apply_fun(params, inputs, rng, dropout):
+    def apply_fun(params, inputs, rng, dropout, train):
         # mask missing variables through forward pass
-        nan_mask = jnp.where(jnp.isnan(inputs), 0.0, 1.0).reshape((1,-1))
-        # replace nans in data to -1 to preserve output, will still be masked
-        inputs = jnp.nan_to_num(inputs, nan=-1.0)
+        rng, key = random.split(rng)
+        rand_noise = random.normal(key, inputs.shape)
+        # replace nans in data to with random noise if training
+        if train:
+            inputs = jnp.where(jnp.isnan(inputs), rand_noise, inputs)
+            nan_mask = jnp.ones(inputs.shape).reshape((-1,1))
+        else:
+            nan_mask = jnp.where(jnp.isnan(inputs), 0.0, 1.0).reshape((-1,1))
+            inputs = jnp.nan_to_num(inputs, nan=-1.0)
         # use concrete dropout to further drop features (f,)
         if dropout:
-            probs = jax.nn.sigmoid(params["logits"])
+            probs = jax.nn.sigmoid(params["fk"]["l1_logit"])
             rng, unif_rng = random.split(rng)
-            unif_noise = random.uniform(unif_rng, (features,))
+            unif_noise = random.uniform(unif_rng, params["fk"]["l1_logit"].shape)
             drop_prob = (jnp.log(probs + eps)
                             - jnp.log(1.0 - probs + eps)
                             + jnp.log(unif_noise + eps)
@@ -170,12 +170,12 @@ def MaskedNeuralNet(
         else:
             mask = nan_mask
 
-        x = l1(params["l1"], inputs, mask=mask)
-        z = fk(params["fk"], x)
+        rng, key = random.split(rng)
+        z = fk(params["fk"], inputs, mask1=mask, rng=key)
         logits = g(params["g"], z)
         return jnp.squeeze(logits), z, z, z
 
-    vapply = jax.vmap(apply_fun, in_axes=(None, 0, 0, None), out_axes=(0, 0, 0, 0) )
+    vapply = jax.vmap(apply_fun, in_axes=(None, 0, 0, None, None), out_axes=(0, 0, 0, 0) )
 
     return init_fun, vapply
 
@@ -282,6 +282,7 @@ def MixtureModel(
         features,
         d_model=32,
         embed_hidden_layers=2,
+        embed_activation=relu,
         dec_activation=relu,
         net_hidden_size=64,
         net_hidden_layers=5,
@@ -294,8 +295,11 @@ def MixtureModel(
         **kwargs
     ):
     # temp = 1 / (features - 1)
-    init_embed, embedf, embedr = Embed(
-        features, d_model, W_init=jax.nn.initializers.ones, b_init=zeros
+    #init_embed, embedf, embedr = Embed(
+    #    features, d_model, W_init=jax.nn.initializers.ones, b_init=zeros
+    #)
+    init_net1, net1 = NeuralNetGeneral(
+        features, 1, d_model, d_model, embed_hidden_layers, W_init=W_init, b_init=b_init, activation=embed_activation
     )
     init_biject, bijectf, bijectr = Bijector(
         features, d_model, embed_hidden_layers, W_init=W_init, b_init=b_init, tactivation=jax.nn.softplus, sactivation=jax.nn.tanh
@@ -310,19 +314,7 @@ def MixtureModel(
 
         params = {}
         rng, key = random.split(rng)
-        params["embed"] = init_embed(key)
-
-        rng, key = random.split(rng)
-        params["bijector"] = init_biject(key)
-
-        rng, key = random.split(rng)
-        params["bijector2"] = init_biject(key)
-
-        rng, key = random.split(rng)
-        params["bijector3"] = init_biject(key)
-
-        rng, key = random.split(rng)
-        params["bijector4"] = init_biject(key)
+        params["embed"] = init_net1(key)
 
         rng, key = random.split(rng)
         params["attn"] = init_attn(key)
@@ -331,7 +323,7 @@ def MixtureModel(
         params["outnet"] = init_out(key)
 
         rng, key = random.split(rng)
-        params["logits"] = jnp.zeros((1, features))
+        params["logits"] = jnp.ones((1, features)) * 0.
 
         rng, key = random.split(rng)
         params["feat"] = W_init(key, (features, d_model))
@@ -372,9 +364,7 @@ def MixtureModel(
 
         # apply embedding to input of (features)
         x = inputs[..., None]
-        z0_f = embedf(params["embed"], x) # (f, ndim)
-        zk_f = bijectf(params["bijector"], z0_f) #  (f, ndim)
-        zk_f = bijectf(params["bijector2"], z0_f + zk_f) #  (f, ndim)
+        zk_f = net1(params["embed"], x) #  (f, ndim)
         zk_f = (zk_f * jnp.transpose(mask, (1, 0)))
         attn = pattn(
             jnp.transpose(

UAT/models/scikit_wrapper.py
@@ -12,9 +12,9 @@ from jax.experimental.optimizers import l2_norm
 # from UAT.models.models import EnsembleModel
 # from UAT.models.models import AttentionModel as Model
 # from UAT.models.models import AttentionModel2 as Model
-from UAT.models.models import MixtureModel as Model
+# from UAT.models.models import MixtureModel as Model
 # from UAT.models.models import CentroidCluster as Model
-# from UAT.models.models import MaskedNeuralNet as Model
+from UAT.models.models import MaskedNeuralNet as Model
 from UAT.training.train import training_loop
 from UAT.training.gradinit import gradinit_loop
 from UAT.training.unsupervised import unsupervised_loop
@@ -202,7 +202,7 @@ class UAT:
         # jitted forward pass
         @jax.jit
         def forward(params, x_batch, rng):
-            out = self.apply_fun(params, x_batch, rng, sample)
+            out = self.apply_fun(params, x_batch, rng, sample, False)
             return out[0]
 
         for tbatch in batches:
@@ -220,7 +220,7 @@ class UAT:
                 key = random.split(self.key, X.shape[0] + 1)
                 self.key = key[0]
                 key = key[1:]
-                out = self.apply_fun(self.params, X, key, sample)
+                out = self.apply_fun(self.params, X, key, sample, False)
             else:
                 out = self.batch_forward(X)
 
@@ -236,7 +236,7 @@ class UAT:
         if self.posterior_params["name"] == "MAP":
             if X.shape[0] <= batch_size:
                 rng_placeholder = jnp.ones((X.shape[0],2))
-                out = self.apply_fun(self.params, X, rng_placeholder, sample)
+                out = self.apply_fun(self.params, X, rng_placeholder, sample, False)
             else:
                  out = self.batch_forward(X, sample)
             out = out[0]

UAT/scores/functions.py
@@ -66,6 +66,37 @@ def cross_entropy(
             return loss, loss_dict
     return loss_fun
 
+def cross_entropy_conc(
+    classes,
+    dropout_reg=1e-4
+    ):
+    def loss_fun(params, output, labels):
+            logits = output[0]
+            one_hot = jax.nn.one_hot(labels, classes)
+            probs = jax.nn.softmax(logits)
+            # probs = jnp.mean(jax.nn.sigmoid(jnp.stack(logits, axis=0)), axis=0)
+            @jax.vmap
+            def cross_entropy(probs, labels):
+                return -(labels * jnp.log(probs + 1e-7) 
+                        + (1-labels) * jnp.log(1 - probs + 1e-7)).sum()
+
+            ce = cross_entropy(probs, one_hot).mean()
+            p_drop = jax.nn.sigmoid(jnp.concatenate([
+                params["fk"]["l1_logit"].flatten(),
+                params["fk"]["hidden"]["lgts"].flatten()]))
+            entropy = p_drop * jnp.log(p_drop + 1e-7)
+            entropy += (1.0 - p_drop) * jnp.log(1.0 - p_drop + 1e-7)
+            entropy = entropy.mean()
+            loss = ce + dropout_reg*entropy
+
+            loss_dict = {
+                "loss":ce,
+                "ent":entropy
+                }
+
+            return loss, loss_dict
+    return loss_fun
+
 def dual(
     classes,
     l2_reg=1e-2,
@@ -90,8 +121,8 @@ def dual(
             ce = cross_entropy(probs, one_hot).mean()
             ce_all = (cross_entropy(probs_all, one_hot[:, None, :])
                       * mask).mean()
-            entropy = p_drop * jnp.log(p_drop + 1e-7)
-            entropy += (1.0 - p_drop) * jnp.log(1.0 - p_drop + 1e-7)
+            entropy = p_drop * jnp.log(1.0 - p_drop + 1e-4)
+            entropy += (1.0 - p_drop) * jnp.log(1.0 - p_drop + 1e-4)
             entropy = entropy.mean()
             mse = jnp.mean(jnp.square(zk_f))
             loss = ce + ce_all + dropout_reg*entropy + msereg*mse

UAT/training/train.py
@@ -63,13 +63,13 @@ def training_loop(
         swa = False
 
     def loss(params, x_batch, y_batch, rng, samp):
-        out = model_fun(params, x_batch, rng, samp)
+        out = model_fun(params, x_batch, rng, samp, True)
         loss, _ = loss_fun(params, out, y_batch)
         return loss
 
     @jax.jit
     def metrics(params, x_batch, y_batch, rng):
-        out = model_fun(params, x_batch, rng, False)
+        out = model_fun(params, x_batch, rng, True, False)
         _, metric_dict = metric_fun(params, out, y_batch)
         return metric_dict
 
@@ -200,7 +200,8 @@ def training_loop(
     perform_test = ((X_test is not None) and (y_test is not None)) and early_stopping is not None
     if perform_test:
         temp_row = np.minimum(20, X_test.shape[0])
-        key_ = jnp.ones((temp_row, 2))
+        rng, key = random.split(rng, 2)
+        key_ = random.split(key, temp_row).reshape((temp_row, 2))
         metric_store_master = metrics(params_, X_test[:temp_row, ...], y_test[:temp_row], key_)
         metric_store_master["loss"] = np.inf
         metric_store_master["counter"] = 0
@@ -243,7 +244,9 @@ def training_loop(
                     # calculate list of metrics over 10 batches of the test set
                     test_dict = dict(zip(metric_store.keys(), [0.0]*len(metric_store.keys())))
                     for tbatch in test_batches[:np.minimum(10, len(test_batches))]:
-                        key_ = jnp.ones((X_test[np.array(tbatch), ...].shape[0], 2))
+                        rng, key = random.split(rng, 2)
+                        key_ = random.split(key, len(tbatch)).reshape((len(tbatch), 2))
+                        # key_ = jnp.ones((X_test[np.array(tbatch), ...].shape[0], 2))
                         temp = metrics(params_, X_test[np.array(tbatch), ...], y_test[np.array(tbatch)], key_)
                         for k in temp.keys():
                             test_dict[k] += temp[k]
@@ -280,13 +283,17 @@ def training_loop(
             # initialize some training metrics
             if step <= 1:
                 params_ = jax.device_get(jax.tree_map(lambda x: x[0], params))
-                metrics_ewa = metrics(params_, batch_x.reshape((batch_size,)+xbs[1:]), batch_y.reshape((batch_size,-1)), None)
+                rng, key = random.split(rng, 2)
+                key_ = random.split(key, batch_size).reshape((batch_size, 2))
+                metrics_ewa = metrics(params_, batch_x.reshape((batch_size,)+xbs[1:]), batch_y.reshape((batch_size,-1)), key_)
                 metrics_ewa["step"]=step
 
             pbar2.update(1)
             if step % output_freq == 0:
                 params_ = jax.device_get(jax.tree_map(lambda x: x[0], params))
-                metrics_dict = metrics(params_, batch_x.reshape((batch_size,)+xbs[1:]), batch_y.reshape((batch_size,-1)), None)
+                rng, key = random.split(rng, 2)
+                key_ = random.split(key, batch_size).reshape((batch_size, 2))
+                metrics_dict = metrics(params_, batch_x.reshape((batch_size,)+xbs[1:]), batch_y.reshape((batch_size,-1)), key_)
                 metrics_dict["step"]=step
                 metrics_ewa = jax.tree_map(lambda x, y : 0.1 * y + 0.9 * x, metrics_ewa, metrics_dict)
                 metrics_ewa["step"]=step

benchmarkaux/openmlrun.py
@@ -16,13 +16,13 @@ if IN_COLAB:
 else:
   from tqdm import tqdm
 
-def dimRepeat(x, t = 1):
+def dimRepeat(x, t = 10):
     return jnp.reshape(
         jnp.repeat(x[None, ...], t, 0),
         (-1, x.shape[1])
     )
 
-def meanRepeats(x, t = 1):
+def meanRepeats(x, t = 10):
     return jnp.mean(x.reshape((t, -1, x.shape[1])), 0)
 
 
@@ -97,9 +97,9 @@ def run(
                 test_complete=test_complete,
                 split=0.2,
                 rng_key=key,
-                prop=0.5,
+                prop=0.8,
                 corrupt=corrupt,
-                cols_miss=int(X.shape[1] * 0.90)
+                cols_miss=int(X.shape[1] * 0.80)
             )
         print(diagnostics)
         count += 1
@@ -153,11 +153,11 @@ def run(
             # assess performance of models on test set and store metrics
             # predict prob will output 
             if task == "Supervised Regression":
-                # output = meanRepeats(model.predict(dimRepeat(X_test)))
-                output = model.predict(X_test)
+                output = meanRepeats(model.predict(dimRepeat(X_test), sample=True))
+                # output = model.predict(X_test)
             elif task == "Supervised Classification":
-                # output = meanRepeats(model.predict_proba(dimRepeat(X_test)))
-                output = model.predict_proba(X_test)
+                output = meanRepeats(model.predict_proba(dimRepeat(X_test), sample=True))
+                # output = model.predict_proba(X_test)
             # calculate performance metrics
             for rm in relevant_metrics:
                 if rm == "accuracy":