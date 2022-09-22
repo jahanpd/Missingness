b39c4a12100478e45a6d3c3d94cf292c337b9925
jahanpd <jahan.pd@gmail.com>
2022-09-22 12:44:52 +1000

    critical difference

---
+++ cd-diagram/orange.py
@@ -0,0 +1,142 @@
+import numpy as np
+import pandas as pd
+import matplotlib
+import itertools
+# matplotlib.use('agg')
+import matplotlib.pyplot as plt
+
+matplotlib.rcParams['font.family'] = 'sans-serif'
+matplotlib.rcParams['font.sans-serif'] = 'Arial'
+
+import operator
+import math
+from scipy.stats import wilcoxon
+from scipy.stats import friedmanchisquare
+import networkx
+import Orange
+
+
+def wilcoxon_holm(alpha=0.05, df_perf=None):
+    """
+    Applies the wilcoxon signed rank test between each pair of algorithm and then use Holm
+    to reject the null's hypothesis
+    """
+    print(pd.unique(df_perf['classifier_name']))
+    # count the number of tested datasets per classifier
+    df_counts = pd.DataFrame({'count': df_perf.groupby(
+        ['classifier_name']).size()}).reset_index()
+    # get the maximum number of tested datasets
+    max_nb_datasets = df_counts['count'].max()
+    # get the list of classifiers who have been tested on nb_max_datasets
+    classifiers = list(df_counts.loc[df_counts['count'] == max_nb_datasets]
+                       ['classifier_name'])
+    # test the null hypothesis using friedman before doing a post-hoc analysis
+    friedman_p_value = friedmanchisquare(*(
+        np.array(df_perf.loc[df_perf['classifier_name'] == c]['accuracy'])
+        for c in classifiers))[1]
+    if friedman_p_value >= alpha:
+        # then the null hypothesis over the entire classifiers cannot be rejected
+        print('the null hypothesis over the entire classifiers cannot be rejected')
+    # get the number of classifiers
+    m = len(classifiers)
+    # init array that contains the p-values calculated by the Wilcoxon signed rank test
+    p_values = []
+    # loop through the algorithms to compare pairwise
+    for i in range(m - 1):
+        # get the name of classifier one
+        classifier_1 = classifiers[i]
+        # get the performance of classifier one
+        perf_1 = np.array(df_perf.loc[df_perf['classifier_name'] == classifier_1]['accuracy']
+                          , dtype=np.float64)
+        for j in range(i + 1, m):
+            # get the name of the second classifier
+            classifier_2 = classifiers[j]
+            # get the performance of classifier one
+            perf_2 = np.array(df_perf.loc[df_perf['classifier_name'] == classifier_2]
+                              ['accuracy'], dtype=np.float64)
+            # calculate the p_value
+            p_value = wilcoxon(perf_1, perf_2, zero_method='wilcox', alternative="two-sided", mode="approx")[1]
+            # appen to the list
+            p_values.append((classifier_1, classifier_2, p_value, False))
+    # get the number of hypothesis
+    k = len(p_values)
+    # sort the list in acsending manner of p-value
+    p_values.sort(key=operator.itemgetter(2))
+    print(p_values)
+    # loop through the hypothesis
+    for i in range(k):
+        # correct alpha with holm
+        # new_alpha = float(alpha / float(k - i))
+        # correct alpha with benjamini-hochberg
+        # new_alpha = float((i+1.0)*alpha / float(k))
+        new_alpha = 0.05
+        # new_alpha = alpha
+        # test if significant after holm's correction of alpha
+        if p_values[i][2] <= new_alpha:
+            p_values[i] = (p_values[i][0], p_values[i][1], p_values[i][2], True)
+        else:
+            # stop
+            break
+    # compute the average ranks to be returned (useful for drawing the cd diagram)
+    # sort the dataframe of performances
+    sorted_df_perf = df_perf.loc[df_perf['classifier_name'].isin(classifiers)]. \
+        sort_values(['classifier_name', 'dataset_name'])
+    # get the rank data
+    rank_data = np.array(sorted_df_perf['accuracy']).reshape(m, max_nb_datasets)
+
+    # create the data frame containg the accuracies
+    df_ranks = pd.DataFrame(data=rank_data, index=np.sort(classifiers), columns=
+    np.unique(sorted_df_perf['dataset_name']))
+
+    # number of wins
+    dfff = df_ranks.rank(ascending=False)
+    print(dfff[dfff == 1.0].sum(axis=1))
+
+    # average the ranks
+    average_ranks = df_ranks.rank(ascending=False).mean(axis=1).sort_values(ascending=False)
+    # return the p-values and the average ranks
+    return p_values, average_ranks, max_nb_datasets
+
+
+def draw_cd_diagram(df_perf, title="Accuracy", labels=True, savename=None):
+    p_values, average_ranks, max_nb_datasets = wilcoxon_holm(alpha=0.05, df_perf=df_perf)
+    print("ranks", average_ranks.values)
+    print("ranks", list(average_ranks.index))
+    print("pvalues",p_values)
+    print("maxnb", max_nb_datasets)
+    cd = Orange.evaluation.compute_CD(average_ranks.values, len(df_perf)) #tested on 30 datasets
+    Orange.evaluation.graph_ranks(average_ranks.values, average_ranks.index, cd=cd, width=6, textspace=1.5, reverse=True)
+    plt.title(title, y=0.9, x=0.5)
+    if savename is not None:
+        plt.savefig(savename,bbox_inches='tight')
+    else:
+        plt.show()
+
+print("TESTING")
+
+for missingness in ["MNAR", "MAR", "MCAR"]:
+    raw = pd.read_pickle('../results/openml/openml_nll.pickle').dropna()
+    raw = raw.set_index((" ", " ", "Dataset"))
+    # build 3 column input dataframe
+    df_perf = {
+        'classifier_name':[],
+        'dataset_name':[],
+        'accuracy':[]
+    }
+
+    print(raw.index)
+
+    for r in list(itertools.product(["LSAM", "LightGBM"], ["None", "Simple", "Iterative", "Miceforest"], raw.index)):
+        name = "LSAM" if r[0] == "LSAM" else "LightGBM"
+        if r[1] == "None":
+            df_perf['classifier_name'].append(name)
+        else:
+            df_perf['classifier_name'].append(name + "+" + r[1])
+        df_perf['dataset_name'].append(r[2])
+        df_perf['accuracy'].append(float(raw.loc[r[2]][missingness][r[1]][r[0]]))
+
+    df_perf = pd.DataFrame(df_perf)
+    print(df_perf)
+    draw_cd_diagram(df_perf=df_perf, title='Change in NLL', labels=True)
+
+
