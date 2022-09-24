83e039a96e8a78ffd397033e620b8e811d0da52a
jahanpd <jahan.pd@gmail.com>
2022-09-24 11:53:36 +1000

    diagram simplified

--- cd-diagram/orange.py
+++ cd-diagram/orange.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib
 import itertools
 # matplotlib.use('agg')
+import os
 import matplotlib.pyplot as plt
 
 matplotlib.rcParams['font.size'] = 13
@@ -15,20 +16,15 @@ import networkx
 import Orange
 
 
-def wilcoxon_holm(alpha=0.05, df_perf=None):
+def get_ranks(alpha=0.05, df_perf=None):
     """
-    Applies the wilcoxon signed rank test between each pair of algorithm and then use Holm
-    to reject the null's hypothesis
+    Determine the ranks after a friedman test
     """
     print(pd.unique(df_perf['classifier_name']))
     # count the number of tested datasets per classifier
     df_counts = pd.DataFrame({'count': df_perf.groupby(
         ['classifier_name']).size()}).reset_index()
-    # get the maximum number of tested datasets
-    max_nb_datasets = df_counts['count'].max()
-    # get the list of classifiers who have been tested on nb_max_datasets
-    classifiers = list(df_counts.loc[df_counts['count'] == max_nb_datasets]
-                       ['classifier_name'])
+    classifiers = list(df_counts['classifier_name'])
     # test the null hypothesis using friedman before doing a post-hoc analysis
     friedman_p_value = friedmanchisquare(*(
         np.array(df_perf.loc[df_perf['classifier_name'] == c]['accuracy'])
@@ -38,50 +34,12 @@ def wilcoxon_holm(alpha=0.05, df_perf=None):
         print('the null hypothesis over the entire classifiers cannot be rejected')
     # get the number of classifiers
     m = len(classifiers)
-    # init array that contains the p-values calculated by the Wilcoxon signed rank test
-    p_values = []
-    # loop through the algorithms to compare pairwise
-    for i in range(m - 1):
-        # get the name of classifier one
-        classifier_1 = classifiers[i]
-        # get the performance of classifier one
-        perf_1 = np.array(df_perf.loc[df_perf['classifier_name'] == classifier_1]['accuracy']
-                          , dtype=np.float64)
-        for j in range(i + 1, m):
-            # get the name of the second classifier
-            classifier_2 = classifiers[j]
-            # get the performance of classifier one
-            perf_2 = np.array(df_perf.loc[df_perf['classifier_name'] == classifier_2]
-                              ['accuracy'], dtype=np.float64)
-            # calculate the p_value
-            p_value = wilcoxon(perf_1, perf_2, zero_method='wilcox', alternative="two-sided", mode="approx")[1]
-            # appen to the list
-            p_values.append((classifier_1, classifier_2, p_value, False))
-    # get the number of hypothesis
-    k = len(p_values)
-    # sort the list in acsending manner of p-value
-    p_values.sort(key=operator.itemgetter(2))
-    print(p_values)
-    # loop through the hypothesis
-    for i in range(k):
-        # correct alpha with holm
-        # new_alpha = float(alpha / float(k - i))
-        # correct alpha with benjamini-hochberg
-        # new_alpha = float((i+1.0)*alpha / float(k))
-        new_alpha = 0.05
-        # new_alpha = alpha
-        # test if significant after holm's correction of alpha
-        if p_values[i][2] <= new_alpha:
-            p_values[i] = (p_values[i][0], p_values[i][1], p_values[i][2], True)
-        else:
-            # stop
-            break
     # compute the average ranks to be returned (useful for drawing the cd diagram)
     # sort the dataframe of performances
     sorted_df_perf = df_perf.loc[df_perf['classifier_name'].isin(classifiers)]. \
         sort_values(['classifier_name', 'dataset_name'])
     # get the rank data
-    rank_data = np.array(sorted_df_perf['accuracy']).reshape(m, max_nb_datasets)
+    rank_data = np.array(sorted_df_perf['accuracy']).reshape(m, -1)
 
     # create the data frame containg the accuracies
     df_ranks = pd.DataFrame(data=rank_data, index=np.sort(classifiers), columns=
@@ -94,12 +52,12 @@ def wilcoxon_holm(alpha=0.05, df_perf=None):
     # average the ranks
     average_ranks = df_ranks.rank(ascending=False).mean(axis=1).sort_values(ascending=False)
     # return the p-values and the average ranks
-    return p_values, average_ranks, max_nb_datasets
+    return average_ranks
 
 
 def draw_cd_diagram(df_perf, title="Accuracy", labels=True, savename=None):
-    p_values, average_ranks, max_nb_datasets = wilcoxon_holm(alpha=0.05, df_perf=df_perf)
-    cd = Orange.evaluation.compute_CD(average_ranks.values, len(df_perf), alpha="0.05", test="nemenyi") #tested on 30 datasets
+    average_ranks = get_ranks(alpha=0.05, df_perf=df_perf)
+    cd = Orange.evaluation.compute_CD(average_ranks.values, len(df_perf), alpha="0.05", test="nemenyi") 
     Orange.evaluation.graph_ranks(average_ranks.values, average_ranks.index, cd=cd, width=4, textspace=0.5, reverse=True)
     # plt.title(title, y=0.9, x=0.5)
     if savename is not None:
